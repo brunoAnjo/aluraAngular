952733b00924ba360b17ac1098ddef15ec04fdb7
= <brunoanjo18@hotmail.com>
2020-01-16 13:51:36 -0300

    implementando a logica do grid

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -9,10 +9,22 @@ import { Photo } from '../../photo/Photo';
 export class PhotosComponent implements OnInit {
 
   @Input() photos: Photo[] = [];
-  
+  rows: any[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.rows = this.goupColumns(this.photos);
+  }
+
+  goupColumns(photos: Photo[]){
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index+=3){
+        newRows.push(photos.slice(index, index +3));
+    }
+
+    return newRows;
   }
 
 }
