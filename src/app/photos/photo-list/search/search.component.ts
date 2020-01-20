78cdcdbeff4475688d7fa0e63fee1f8645865ee4
= <brunoanjo18@hotmail.com>
2020-01-20 11:11:52 -0300

    Output property

--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from "@angular/core";
 import { Subject } from "rxjs";
-import { debounceTime } from 'rxjs/operators';
+import { debounceTime, filter } from 'rxjs/operators';
+
 
 @Component({
     selector: 'ap-search',
@@ -9,12 +10,15 @@ import { debounceTime } from 'rxjs/operators';
 
 export class SearchComponent implements OnInit, OnDestroy {
 
+    @Output() onTyping = new EventEmitter<string>();
+    @Input() value: string = '';
     debounce: Subject<string> = new Subject<string>();
 
     ngOnInit(): void {
 
         this.debounce
-            .pipe(debounceTime(300));
+            .pipe(debounceTime(300))
+            .subscribe(filter => this.onTyping.emit(filter));
     }
 
     ngOnDestroy(): void {
